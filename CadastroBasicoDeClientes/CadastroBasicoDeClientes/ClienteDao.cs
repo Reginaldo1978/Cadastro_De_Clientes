e55463235b55c131fdb88547155ae135617bfda1
Reginaldo de Oliveira Souza <regis020378@hotmail.com>
2023-07-19 18:14:21 -0300

    Criação da classe Cliente para reprentar o banco de dados

--- CadastroBasicoDeClientes/CadastroBasicoDeClientes/ClienteDao.cs
+++ CadastroBasicoDeClientes/CadastroBasicoDeClientes/ClienteDao.cs
@@ -16,15 +16,15 @@ namespace CadastroBasicoDeClientes
             contexto = new Contexto();
         }
 
-        public void Inserir(string nome, string email, string uf, string data) 
+        private void Inserir(Cliente cliente) 
         {
-            var strQuery = string.Format("Insert into Clientes(Nome, Email, UF, DataNascimento) values('{0}', '{1}', '{2}', '{3}')", nome, email, uf, data);
+            var strQuery = string.Format("Insert into Clientes(Nome, Email, UF, DataNascimento) values('{0}', '{1}', '{2}', '{3}')", cliente.Nome, cliente.Email, cliente.UF, cliente.DataNascimento);
             contexto.ExecutaComandoSemRetorno(strQuery);
         }
 
-        public void Alterar(int id, string nome, string email, string uf, string data) 
+        private void Alterar(Cliente cliente) 
         {
-            var strQuery = string.Format("Update Clientes set Nome='{0}', Email='{1}', UF='{2}', DataNascimento='{3}' Where CodCliente={4}", nome, email, uf, data, id);
+            var strQuery = string.Format("Update Clientes set Nome='{0}', Email='{1}', UF='{2}', DataNascimento='{3}' Where CodCliente={4}", cliente.Nome, cliente.Email, cliente.UF, cliente.DataNascimento, cliente.Id);
             contexto.ExecutaComandoSemRetorno(strQuery);
         }
 
@@ -34,10 +34,33 @@ namespace CadastroBasicoDeClientes
             contexto.ExecutaComandoSemRetorno(strQuery);
         }
 
-        public SqlDataReader ListarTodos() 
+        public List<Cliente> ListarTodos() 
         {
-            var strquery = "Select * from Clientes";
-            return contexto.ExecutaComandoComRetorno(strquery);
+            var listaDeClientes = new List<Cliente>();
+            var strQuery = "Select * from Clientes";
+            SqlDataReader dados = contexto.ExecutaComandoComRetorno(strQuery);
+            while (dados.Read())
+            {
+                var cliente = new Cliente
+                {
+                    Id = Convert.ToInt32(dados["CodCliente"]),
+                    Nome = Convert.ToString(dados["Nome"]),
+                    Email = Convert.ToString(dados["Email"]),
+                    UF = Convert.ToString(dados["UF"]),
+                    DataNascimento = Convert.ToDateTime(dados["DataNascimento"])
+                };
+                listaDeClientes.Add(cliente);
+            }
+            return listaDeClientes;
+        }
+
+        //Esse método insere ou altera, se na classe Program for passado um Id ele altera, se não ele insere
+        public void Salvar(Cliente cliente) 
+        {
+            if (cliente.Id > 0)
+                Alterar(cliente);
+            else
+                Inserir(cliente);
         }
     }
 }
--- CadastroBasicoDeClientes/CadastroBasicoDeClientes/Program.cs
+++ CadastroBasicoDeClientes/CadastroBasicoDeClientes/Program.cs
@@ -11,53 +11,35 @@ namespace CadastroBasicoDeClientes
     {
         static void Main(string[] args)
         {
-            Contexto contexto = new Contexto();
             var clienteDao = new ClienteDao();
 
             //==================================Cadastrar======================================
 
             //descomente as linhas abaixo para um novo cadastro
 
-            //Console.Write("Informe o nome do cliente: ");
-            //string nome = Console.ReadLine();
+            Console.Write("Informe o nome do cliente: ");
+            string nome = Console.ReadLine();
 
-            //Console.Write("Informe o email do cliente: ");
-            //string email = Console.ReadLine();
+            Console.Write("Informe o email do cliente: ");
+            string email = Console.ReadLine();
 
-            //Console.Write("Informe a UF do cliente: ");
-            //string uf = Console.ReadLine();
+            Console.Write("Informe a UF do cliente: ");
+            string uf = Console.ReadLine();
 
-            //Console.Write("Informe a data de nascimento do clinte: ");
-            //string data = Console.ReadLine();
+            Console.Write("Informe a data de nascimento do clinte: ");
+            string data = Console.ReadLine();
 
-            //clienteDao.Inserir(nome, email, uf, data);
+            var cliente = new Cliente 
+            {
+                Nome = nome,
+                Email = email,
+                UF = uf,
+                DataNascimento = Convert.ToDateTime(data)
+            };
 
-            //string queryInsert = string.Format("Insert into Clientes (Nome, email, uf, DataNascimento) Values('{0}', '{1}', '{2}', '{3}')", nome, email, uf, data);
-            //contexto.ExecutaComandoSemRetorno(queryInsert);
-
-
-
-            //==============================Alterar======================================
-
-            //Descomente as linhas abaixo no caso de querer alterar uma informação
-
-            //Console.Write("Informe o Id desejado para alteração: ");
-            //int id = Convert.ToInt32(Console.ReadLine());
-
-            //Console.Write("Informe o nome do cliente: ");
-            //string nome = Console.ReadLine();
-
-            //Console.Write("Informe o email do cliente: ");
-            //string email = Console.ReadLine();
-
-            //Console.Write("Informe a UF do cliente: ");
-            //string uf = Console.ReadLine();
-
-            //Console.Write("Informe a data de nascimento do clinte: ");
-            //string data = Console.ReadLine();
-
-            //clienteDao.Alterar(id, nome, email, uf, data);            
-
+            //Para alteração, descomente a linha abaixo, digite o Id do cliente que deseja alterar
+            //cliente.Id = 11; 
+            clienteDao.Salvar(cliente); 
 
             //==============================Excluir======================================
 
@@ -67,21 +49,12 @@ namespace CadastroBasicoDeClientes
             //int id = Convert.ToInt32(Console.ReadLine());
             //clienteDao.Excluir(id);
 
-
             //===============================Listar todos os clientes=====================================
-
-            //SqlDataReader dados = clienteDao.ListarTodos();
-
-            //while (dados.Read())
-            //{
-            //    Console.WriteLine(string.Format(" CodCliente: {0}, Nome: {1}, Email: {2}, UF: {3}, DataNascimento: {4}", dados["CodCliente"], dados["Nome"], dados["Email"], dados["UF"], dados["DataNascimento"]));
-            //}
-            SqlDataReader listaClientes = clienteDao.ListarTodos();
-            foreach (var cliente in listaClientes)
+                        
+            foreach (var cli in clienteDao.ListarTodos())
             {
-                Console.WriteLine(string.Format(" CodCliente: {0}, Nome: {1}, Email: {2}, UF: {3}, DataNascimento: {4}", listaClientes["CodCliente"], listaClientes["Nome"], listaClientes["Email"], listaClientes["UF"], listaClientes["DataNascimento"]));
+                Console.WriteLine(string.Format(" CodCliente: {0}, Nome: {1}, Email: {2}, UF: {3}, DataNascimento: {4}", cli.Id, cli.Nome, cli.Email, cli.UF, cli.DataNascimento));
             }
-
             Console.ReadKey();
         }
     }
